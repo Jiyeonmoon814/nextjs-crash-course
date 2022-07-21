9581900208dcc950f83752afd631cb826b8cb26c
mjyinseoul <mjyinseoul@gmail.com>
2022-07-21 20:22:38 +0100

    api routes and use api N create meta

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import {useRouter} from 'next/router'
+import { server } from '../../../config'
+import Meta from '../../../components/Meta'
 
 const article = ({ article }) => {
     //const router = useRouter()
@@ -7,6 +9,7 @@ const article = ({ article }) => {
 
     return (
         <>
+        <Meta title={article.title} />
             <h1>{article.title}</h1>
             <p>{article.body}</p>
             <br />
@@ -15,8 +18,33 @@ const article = ({ article }) => {
     )
 }
 
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     return {
+//         props : {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+//     const articles = await res.json()
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params : {id : id.toString()}}))
+
+//     return {
+//         paths,
+//         fallback : false 
+//     }
+// }
+
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
 
@@ -28,7 +56,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
 
     const articles = await res.json()
     const ids = articles.map(article => article.id)
