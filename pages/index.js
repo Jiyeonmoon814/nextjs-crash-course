d9bf88ff84ea514b1e1bae7d3975784d90e75f53
mjyinseoul <mjyinseoul@gmail.com>
2022-07-21 18:51:12 +0100

    add Head

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name='keywords' content='web development, programming'></meta>
+      </Head>
       <h1>Welcome to next</h1>
     </div>
   )
