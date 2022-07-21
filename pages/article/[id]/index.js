159455e54d659fc600ce7981d113783921ce1252
mjyinseoul <mjyinseoul@gmail.com>
2022-07-21 20:01:16 +0100

    export a static website with staticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -15,7 +15,7 @@ const article = ({ article }) => {
     )
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
 
     const article = await res.json()
@@ -26,5 +26,19 @@ export const getServerSideProps = async (context) => {
         }
     }
 }
+
+export const getStaticPaths = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+    const ids = articles.map(article => article.id)
+    const paths = ids.map(id => ({params : {id : id.toString()}}))
+
+    return {
+        paths,
+        fallback : false 
+    }
+}
+
 
 export default article
